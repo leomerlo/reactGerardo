3c54ea8e56c1b7daf1c973aa75def8f8964e5e58
Leo Merlo <merloleandro@gmail.com>
2022-06-15 22:33:28 -0300

    Cambiar titulo

--- gerardo/src/components/NavBar.js
+++ gerardo/src/components/NavBar.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import logo from '../assets/logo.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import './NavBar.css';
 
 function NavBar() {
@@ -9,15 +7,15 @@ function NavBar() {
     <header>
       <div className="flex justify-between m-3 items-center">
         <div className="m-3"> </div>
-        <div className="m-3">
+        <div className="m-3 text-center">
           <img src={logo} className="w-24" alt="logo" />
-          <span className="text-xs">Mushroomania</span>
+          <span className="text-xs">Gerardo</span>
         </div>
         <div className="m-3">
-          <FontAwesomeIcon icon={faCartShopping} />
+          
         </div>
       </div>
-      <div className="flex justify-center m-5">
+      <div className="flex justify-center m-3">
         <ul className='flex'>
           <li><a href="#" className="p-5 text-sm">Home</a></li>
           <li><a href="#" className="p-5 text-sm">Nosotros</a></li>
